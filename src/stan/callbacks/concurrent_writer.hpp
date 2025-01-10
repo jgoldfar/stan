f2c4c1b7a821959f339247728ae23416fab76716
Stan Jenkins <mc.stanislaw@gmail.com>
2025-01-10 16:06:58 -0500

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- src/stan/callbacks/concurrent_writer.hpp
+++ src/stan/callbacks/concurrent_writer.hpp
@@ -1,15 +1,15 @@
 #ifndef STAN_CALLBACKS_CONCURRENT_WRITER_HPP
 #define STAN_CALLBACKS_CONCURRENT_WRITER_HPP
 
-
 #ifdef STAN_THREADS
 /**
  * Takes a writer and makes it thread safe via multiple queues.
  * At the first write a single busy thread is spawned to write to the writer.
- * This class uses an `std::thread` instead of a tbb task graph because 
- * of deadlocking issues. A deadlock can occur if TBB gives all threads to the parallel for loop,
- * and all threads hit an instance of max capacity. TBB can choose to wait for a thread to finish
- * instead of spinning up the write thread. So to circumvent that issue, we use an std::thread.
+ * This class uses an `std::thread` instead of a tbb task graph because
+ * of deadlocking issues. A deadlock can occur if TBB gives all threads to the
+ * parallel for loop, and all threads hit an instance of max capacity. TBB can
+ * choose to wait for a thread to finish instead of spinning up the write
+ * thread. So to circumvent that issue, we use an std::thread.
  * @tparam Writer A type that inherits from `writer`
  */
 template <typename Writer>
@@ -21,36 +21,40 @@ struct concurrent_writer {
   bool continue_writing_{true};
   std::thread thread_;
   /**
-   * Constructs a concurrent writer from a writer and spins up a thread for writing.
+   * Constructs a concurrent writer from a writer and spins up a thread for
+   * writing.
    * @param writer A writer to write to
    */
-  explicit concurrent_writer(Writer& writer)
-      : writer(writer) {
-        str_messages_.set_capacity(1000);
-        vec_str_messages_.set_capacity(1000);
-        eigen_messages_.set_capacity(1000);
-        thread_ = std::thread([&]() {
-          std::string str;
-          std::vector<std::string> vec_str;
-          Eigen::RowVectorXd eigen;
-          std::size_t max_size = 0;
-          while (continue_writing_ || !(str_messages_.empty() && vec_str_messages_.empty() && eigen_messages_.empty())) {
-            if (str_messages_.try_pop(str)) {
-              writer(str);
-            }
-            if (vec_str_messages_.try_pop(vec_str)) {
-              writer(vec_str);
-            }
-            max_size = std::max(max_size, static_cast<std::size_t>(eigen_messages_.size()));
-            if (eigen_messages_.try_pop(eigen)) {
-              writer(eigen);
-            }
-          }
-        });
+  explicit concurrent_writer(Writer& writer) : writer(writer) {
+    str_messages_.set_capacity(1000);
+    vec_str_messages_.set_capacity(1000);
+    eigen_messages_.set_capacity(1000);
+    thread_ = std::thread([&]() {
+      std::string str;
+      std::vector<std::string> vec_str;
+      Eigen::RowVectorXd eigen;
+      std::size_t max_size = 0;
+      while (continue_writing_
+             || !(str_messages_.empty() && vec_str_messages_.empty()
+                  && eigen_messages_.empty())) {
+        if (str_messages_.try_pop(str)) {
+          writer(str);
+        }
+        if (vec_str_messages_.try_pop(vec_str)) {
+          writer(vec_str);
+        }
+        max_size = std::max(max_size,
+                            static_cast<std::size_t>(eigen_messages_.size()));
+        if (eigen_messages_.try_pop(eigen)) {
+          writer(eigen);
+        }
       }
+    });
+  }
   /**
    * Place a value in a queue for writing.
-   * @tparam T Either an `std::vector<std::string|double>`, an Eigen vector, or a string
+   * @tparam T Either an `std::vector<std::string|double>`, an Eigen vector, or
+   * a string
    * @param t A value to put on a queue
    */
   template <typename T>
@@ -71,9 +75,7 @@ struct concurrent_writer {
       static_assert(1, "Unsupported type passed to concurrent_writer");
     }
   }
-  void operator()() {
-    str_messages_.push(writer.get().comment_prefix());
-  }
+  void operator()() { str_messages_.push(writer.get().comment_prefix()); }
   void wait() {
     continue_writing_ = false;
     thread_.join();
@@ -83,17 +85,13 @@ struct concurrent_writer {
 template <typename Writer>
 struct concurrent_writer {
   std::reference_wrapper<Writer> writer;
-  explicit concurrent_writer(Writer& writer)
-      : writer(writer) {}
+  explicit concurrent_writer(Writer& writer) : writer(writer) {}
   template <typename T>
   void operator()(T&& t) {
     writer(std::forward<T>(t));
   }
-  void operator()() {
-    writer();
-  }
-  void wait() {
-  }
+  void operator()() { writer(); }
+  void wait() {}
 };
 #endif
 
